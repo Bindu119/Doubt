7ceba054d4c07b1055468f227a48240ba175e67b
HussainMadarwala53 <70691828+HussainMadarwala53@users.noreply.github.com>
2021-03-23 21:01:17 +0530

    Update sling.js

--- sling.js
+++ sling.js
@@ -1,29 +1,27 @@
 class Sling{
-    constructor(bodyA,pointB){
-    var options={
-        bodyA:bodyA,
-        pointB:pointB,
-        stiffness:0.04,
-        length:10
-    }
-    this.pointB=pointB;
-    this.slingShot=Constraint.create(options);
-    World.add(world,this.slingShot);
-    }
-    attach(body){
-        this.slingShot.bodyA=body;
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
     }
 
     fly(){
-        this.slingShot.bodyA=null;
+        this.sling.bodyA = null;
     }
 
     display(){
-        if(this.slingShot.bodyA){
-            var pointA = this.slingShot.bodyA.position;
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
+    
 }
